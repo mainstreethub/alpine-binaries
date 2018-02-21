3b9b8e69bc434925c0681cd47a2bc2112c37ebd8
Alex Wenckus <alex.wenckus@mainstreethub.com>
2018-02-21 11:43:59 -0600

    Remove for a PR

--- dynamodb-local/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-docker build ./ -t dynamodb-local
-docker run --entrypoint /bin/sh --rm -iv ${PWD}:/host-volume dynamodb-local -s <<EOF
-chown $(id -u):$(id -g) /DynamoDBLocal_lib/libsqlite4java-linux-amd64.so
-cp -a /DynamoDBLocal_lib/libsqlite4java-linux-amd64.so /host-volume
-EOF
